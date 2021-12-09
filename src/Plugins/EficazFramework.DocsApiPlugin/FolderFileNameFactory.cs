5877d26f09e62a33f508d6546052b4a7ccf288ca
Henrique Clausing <hclausing@hotmail.com>
2021-12-09 14:39:54 -0300

    - (Docs) Rollback da tentativa de utilizar a versão 10.0 do c# em plugin.

--- src/Plugins/EficazFramework.DocsApiPlugin/FolderFileNameFactory.cs
+++ src/Plugins/EficazFramework.DocsApiPlugin/FolderFileNameFactory.cs
@@ -8,53 +8,55 @@ using DefaultDocumentation;
 using DefaultDocumentation.Api;
 using DefaultDocumentation.Models;
 
-namespace EficazFramework.DocsApiPlugin;
-
-[ExcludeFromCodeCoverage]
-public sealed class FolderFileNameFactory : IFileNameFactory
+namespace EficazFramework.DocsApiPlugin
 {
-    public string Name => "Folder";
-
-    public void Clean(IGeneralContext context)
+    [ExcludeFromCodeCoverage]
+    public sealed class FolderFileNameFactory : IFileNameFactory
     {
-        context.Settings.Logger.Debug($"Cleaning output folder \"{context.Settings.OutputDirectory}\"");
+        public string Name => "Folder";
 
-        if (context.Settings.OutputDirectory.Exists)
+        public void Clean(IGeneralContext context)
         {
-            IEnumerable<FileInfo> files = context.Settings.OutputDirectory.EnumerateFiles("*.md", SearchOption.AllDirectories).Where(f => !string.Equals(f.Name, "readme.md", StringComparison.OrdinalIgnoreCase));
+            context.Settings.Logger.Debug($"Cleaning output folder \"{context.Settings.OutputDirectory}\"");
 
-            int i;
-
-            foreach (FileInfo file in files)
+            if (context.Settings.OutputDirectory.Exists)
             {
-                i = 3;
-            start:
-                try
+                IEnumerable<FileInfo> files = context.Settings.OutputDirectory.EnumerateFiles("*.md", SearchOption.AllDirectories).Where(f => !string.Equals(f.Name, "readme.md", StringComparison.OrdinalIgnoreCase));
+
+                int i;
+
+                foreach (FileInfo file in files)
                 {
-                    file.Delete();
-                }
-                catch
-                {
-                    if (--i > 0)
+                    i = 3;
+                start:
+                    try
                     {
-                        Thread.Sleep(100);
-                        goto start;
+                        file.Delete();
                     }
+                    catch
+                    {
+                        if (--i > 0)
+                        {
+                            Thread.Sleep(100);
+                            goto start;
+                        }
 
-                    throw;
+                        throw;
+                    }
                 }
-            }
 
-            i = 3;
-            while (files.Any() && i-- > 0)
-            {
-                Thread.Sleep(1000);
+                i = 3;
+                while (files.Any() && i-- > 0)
+                {
+                    Thread.Sleep(1000);
+                }
             }
         }
-    }
 
-    public string GetFileName(IGeneralContext context, DocItem item)
-    {
-        return (item is AssemblyDocItem ? item.FullName : string.Join("/", item.GetParents().Skip(1).Select(p => p.Name).Concat(Enumerable.Repeat(item.Name, 1)))) + ".md";
+        public string GetFileName(IGeneralContext context, DocItem item)
+        {
+            return (item is AssemblyDocItem ? item.FullName : string.Join("/", item.GetParents().Skip(1).Select(p => p.Name).Concat(Enumerable.Repeat(item.Name, 1)))) + ".md";
+        }
     }
 }
+
