adec47e7448cdd061689dae868a3ed6ec23008f3
Henrique Clausing <hclausing@hotmail.com>
2022-01-04 15:14:09 -0300

    - Removidas strings dos recursos .resx da análise de cobertuda de código.

--- src/SolutionScripts/Tests.psm1
+++ src/SolutionScripts/Tests.psm1
@@ -1,6 +1,6 @@
 # Executa os testes e contrói a tabela de cobertura de código do Coverlet
 Function Coverage() {
 	Process {
-		dotnet test --no-build /p:CollectCoverage=true
+		dotnet test --no-build /p:CollectCoverage=true /p:Exclude=[*]EficazFramework.Resources.Strings.*
 	}
 }
