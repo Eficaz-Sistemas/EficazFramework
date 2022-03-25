822aff1735308428fd25641e1820e889f3285ca2
Henrique Clausing <hclausing@gmail.com>
2022-03-25 09:49:20 -0300

    - (Blazor) Implementado método estático .ShowAsync() para ViewModelDialog, visando facilitar a chamada de diálogos.

---
+++ src/Tests/Web/EficazFramework.Tests.Blazor.Views/Pages/Components/Dialogs/ViewModelDialog.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EficazFramework.Tests.Blazor.Views.Pages.Components.Dialogs;
+
+public partial class ViewModelDialog
+{
+    [Inject] MudBlazor.IDialogService DialogService {get; set;}
+
+    private string _title = "MessageBox Title";
+    private string _content = "Hello World!";
+    private Events.MessageType _type = Events.MessageType.Default;
+    private Events.MessageButtons _buttons = Events.MessageButtons.OK;
+
+    private async Task ShowDialog()
+    {
+        Events.MessageEventArgs e = new()
+        {
+            Title = _title,
+            Content = _content,
+            Buttons = _buttons,
+            Type = _type
+        };
+        await EficazFramework.Components.Dialogs.ViewModelDialog.ShowAsync(DialogService, e);
+    }
+}
--- src/Web/EficazFramework.Blazor/Components/Dialogs/ViewModelDialog.razor.cs
+++ src/Web/EficazFramework.Blazor/Components/Dialogs/ViewModelDialog.razor.cs
@@ -6,4 +6,25 @@ public partial class ViewModelDialog
 {
     [CascadingParameter] MudBlazor.MudDialogInstance MudDialog { get; set; }
     [Parameter] public EficazFramework.Events.MessageEventArgs Args { get; set; }
+
+    public static async Task ShowAsync(MudBlazor.IDialogService dialogService,
+                                       Events.MessageEventArgs messageArgs, 
+                                       MudBlazor.DialogParameters? mudDialogParams = null,
+                                       MudBlazor.DialogOptions? mudDialogOptions = null)
+    {
+        if (messageArgs.Type != Events.MessageType.Default)
+            return;
+
+        if (mudDialogParams == null)
+            mudDialogParams = new();
+
+        mudDialogParams?.Add("Args", messageArgs);
+
+        MudBlazor.IDialogReference _dialog = dialogService.Show<ViewModelDialog>(messageArgs.Title, 
+                                                                                 mudDialogParams,
+                                                                                 mudDialogOptions);
+
+        Events.MessageResult result = (Events.MessageResult)(await _dialog.Result).Data;
+        messageArgs.ModalAssist.Release(result);
+    }
 }
