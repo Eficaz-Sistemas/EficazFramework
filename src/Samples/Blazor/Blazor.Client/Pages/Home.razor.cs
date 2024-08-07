0245fe8228605bd18f165db380fff5c57a6269b6
Henrique Clausing <hclausing@gmail.com>
2024-08-07 16:12:25 -0300

    [Blazor] Implementações em aplicação de exemplo.

---
+++ src/Samples/Blazor/Blazor.Client/Pages/Home.razor.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Blazor.Client.Pages;
+
+public partial class Home
+{
+    private EficazFramework.Components.MdiHost? _mdiHost;
+    private EficazFramework.Components.StartMenu? _startMenu;
+
+    protected override Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+            if (!(_startMenu?.IsOpen ?? true))
+                _startMenu?.ToggleOpen();
+
+        return base.OnAfterRenderAsync(firstRender);
+    }
+}
