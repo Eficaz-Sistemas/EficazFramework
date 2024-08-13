971cc8b3a4211ba3001452769b0dc133f9848124
Henrique Clausing <hclausing@gmail.com>
2024-08-13 14:33:08 -0300

    Publish de aplicação exemplo após correção da rolagem de Mdi

--- src/Samples/Blazor/Blazor.Client/wwwroot/service-worker.published.js
+++ src/Samples/Blazor/Blazor.Client/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.0.1.2';
+const CACHE_VERSION = '0.0.1.3';
 
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
